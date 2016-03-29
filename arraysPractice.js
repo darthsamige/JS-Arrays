bfe7650f5313ba7a22a7b50061b7b2ca7dbcb8a2
darthsamige <darthsamige@gmail.com>
2016-03-28 19:32:22 -0600

    first commit

--- arraysPractice.js
+++ arraysPractice.js
@@ -5,7 +5,9 @@ var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item in the given array.
 
   //Code Here
-
+function first(arr){
+    return(arr[0]);
+}
 
 //Next problem
 
@@ -16,7 +18,9 @@ var arr = [40,50,60];
 
 
   //Code Here
-
+function last(arr){
+    return(arr[2]);
+}
 
 //Next Problem
 
@@ -25,6 +29,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
   //Code Here
+  function looper(family){
+      for(i =0; i < family.length; i++){
+          alert(family[i]);
+      }
+  }
 
 
 //Next problem
@@ -35,6 +44,13 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
 
   //Code Here
+function reversedLooper(letters) {
+   for (var i = letters.length - 1; i >= 0; i--) {
+    alert(letters[i]);
+}
+      }
+    
+  
 
 
 //Next Problem
@@ -44,12 +60,25 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
   //Code Here
+  function evenFinder(nums){
+      var out = 0; 
+    for (var i = 0; i < nums.length; i++) {
+        if (! (nums[i] % 2)) {
+            nums[out++] = nums[i]; 
+        }
+    }
+    nums.length = out;
+    return nums;
+}
+      
+     
+  
 
 
 //Next problem
 
 
-var nums = [1,2,34,54,55,34,32,11,19,17,54,66,13];
+var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 //Write a function called divider that is given one argument, numbersArray.
 //Have divider return an Array with the first item in the array being the evens array (all the even values from nums) and the second item in the Array being the odds array(all the odd values from nums).
 
